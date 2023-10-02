f52412ebae26ba49c7038bba86ed282e330a5cfb
Zhi <neozhixuan@gmail.com>
2023-10-02 12:09:24 +0800

    Feature - API Key Leak + Add Files

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 import pickle
 from utils import load_files, tokenize_and_process
-
+import time
 N_COMPONENTS = 100  # You can adjust this value as per your requirements for SVD
 
 
@@ -12,7 +12,7 @@ def main():
     # Check command-line arguments
     if len(sys.argv) != 2:
         sys.exit("Enter your input like this: python preprocessing.py corpus")
-
+    start_time = time.time()
     files = load_files(sys.argv[1])
 
     file_words = {
@@ -44,8 +44,9 @@ def main():
 
     with open("preprocess/latent_semantic_matrix.pkl", "wb") as f:
         pickle.dump(latent_semantic_matrix, f)
-
-    print("Successfully preprocessed all vectors and matrices.")
+    end_time = time.time()
+    print(
+        f"Successfully preprocessed all vectors and matrices with a time of {(end_time - start_time).to} seconds")
 
 
 if __name__ == "__main__":
